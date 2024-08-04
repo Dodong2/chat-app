e246663617036e926695e05365883461e8a002f6
Dodong2 <carlseighartaliodegmail.com>
2024-08-04 22:30:50 +0800

    3rd commit

--- src/components/chat app/chat page/Conversation.tsx
+++ src/components/chat app/chat page/Conversation.tsx
@@ -12,6 +12,83 @@ const Conversation = () => {
       <div className="message sender">
         <p>This is the receiver's message.</p>
       </div>
+      {/* Receiver's message*/}
+      <div className="message receiver">
+        <p>This is the receiver's message.</p>
+      </div>
+      {/* sender message*/}
+      <div className="message sender">
+        <p>This is the receiver's message.</p>
+      </div>
+      {/* Receiver's message*/}
+      <div className="message receiver">
+        <p>This is the receiver's message.</p>
+      </div>
+      {/* sender message*/}
+      <div className="message sender">
+        <p>This is the receiver's message.</p>
+      </div>
+      {/* Receiver's message*/}
+      <div className="message receiver">
+        <p>This is the receiver's message.</p>
+      </div>
+      {/* sender message*/}
+      <div className="message sender">
+        <p>This is the receiver's message.</p>
+      </div>
+      {/* Receiver's message*/}
+      <div className="message receiver">
+        <p>This is the receiver's message.</p>
+      </div>
+      {/* sender message*/}
+      <div className="message sender">
+        <p>This is the receiver's message.</p>
+      </div>
+      {/* Receiver's message*/}
+      <div className="message receiver">
+        <p>This is the receiver's message.</p>
+      </div>
+      {/* sender message*/}
+      <div className="message sender">
+        <p>This is the receiver's message.</p>
+      </div>
+      {/* Receiver's message*/}
+      <div className="message receiver">
+        <p>This is the receiver's message.</p>
+      </div>
+      {/* sender message*/}
+      <div className="message sender">
+        <p>This is the receiver's message.</p>
+      </div>{/* Receiver's message*/}
+      <div className="message receiver">
+        <p>This is the receiver's message.</p>
+      </div>
+      {/* sender message*/}
+      <div className="message sender">
+        <p>This is the receiver's message.</p>
+      </div>{/* Receiver's message*/}
+      <div className="message receiver">
+        <p>This is the receiver's message.</p>
+      </div>
+      {/* sender message*/}
+      <div className="message sender">
+        <p>This is the receiver's message.</p>
+      </div>
+      {/* Receiver's message*/}
+      <div className="message receiver">
+        <p>This is the receiver's message.</p>
+      </div>
+      {/* sender message*/}
+      <div className="message sender">
+        <p>This is the receiver's message.</p>
+      </div>{/* Receiver's message*/}
+      <div className="message receiver">
+        <p>This is the receiver's message.</p>
+      </div>
+      {/* sender message*/}
+      <div className="message sender">
+        <p>This is the receiver's message.</p>
+      </div>
       </div>
     </>
   )
